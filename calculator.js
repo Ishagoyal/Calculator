5c43aeec67dd21c7f1306dc94ad9df9fc19ca667
Isha Goyal <iamishagoyal@gmail.com>
2017-01-14 02:39:52 +0530

    other operation performing

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+// Button animations
 $(document).ready(function(){
   setDisplay(0);
   $('a').click(function(){
@@ -8,89 +9,112 @@ $(document).ready(function(){
   });
 });
 
-var answer=0;
-var num1,num2;
-var display = document.querySelector('input');
-var bool = false;
-var operator = '';
+// Calculator Functionality
+let answer = 0;
+let num1, num2;
+let display = document.querySelector('input');
+let bool = false;
+let operator = '';
 
-var setDisplay = function(num){
-  if(bool === true){
+let setDisplay = function(num){
+  if (bool === true){
     display.value = answer;
   }
-  else{ 
-    if(display.value === '0'){
+  else {
+    if (display.value === '0')
       display.value = num;
-    }
-    else{
+    else
       display.value += num;
-    }
-     
   }
 }
 
-var clicked = document.querySelector('body');
+let clicked = document.querySelector('body');
 clicked.addEventListener('click', evt => {
-  var btnValue = parseInt(evt.target.innerHTML);
+  let btnValue = parseInt(evt.target.innerHTML);
   console.log(operator);
-  if(bool === true){
+  if (bool === true){
     display.value = '';
     bool = false;
   }
-  if(Number.isInteger(btnValue)){
+  if (Number.isInteger(btnValue)){
     setDisplay(btnValue);
-  }
-  else{
+  } else {
     setDisplay('');
-    doIt(evt.target.innerHTML)
+    doIt(evt.target.innerHTML);
   }
 });
 
-var doIt = symbol => {
+let doIt = symbol => {
   switch (symbol){
-    case 'C': clear();
-              break;
-      
-    case '=': if(operator !== ''){
-      num2 = display.value;
-      calculate();
-    }
+    case 'C':
+      clear();
       break;
-      
-     case '+':
-     case 'x':
-     case '-':
-     case '/':
-      if(operator === ''){
+    case '=':
+      if (operator !== ''){
+        num2 = display.value;
+        calculate();
+      }
+      break;
+    case '+':
+    case 'x':
+    case '-':
+    case '/':
+      if (operator === ''){
         num1 = display.value;
         operator = symbol;
-        bool = true;;
-      }
-      else {
+        bool = true;
+      } else {
         num2 = display.value;
         operator = symbol;
         calculate();
       }
       break;
-     }
+    case '.':
+      decimal();
+      break;
+    case '=':
+      equals();
+      break;
+  }
 };
 
+let clear = () => {
+  answer = 0;
+  bool = false;
+  operator = '';
+  display.value = answer;
+};
 
-var calculate = function(){
-  num1=parseFloat(num1);
-  num2=parseFloat(num2);
+let add = () => {
+  num1 = display.value;
+  
+};
+
+let calculate = () => {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   
   switch (operator){
-      
-    case '+': 
+    case '+':
       answer = num1 + num2;
       break;
-                 }
+    case '-':
+      answer = num1 - num2;
+      break;
+    case 'x':
+      answer = num1 * num2;
+      break;
+    case '/':
+      answer = num1 / num2;
+      break;
+    case '=':
+  }
   bool = true;
   setDisplay(answer);
   num1 = answer;
   num2 = undefined;
 };
 
-
-  
+let decimal = () => {
+  if (display.value.indexOf('.') === -1) setDisplay('.');
+};
